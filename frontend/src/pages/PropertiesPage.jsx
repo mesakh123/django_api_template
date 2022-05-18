40b068cd585fad27804dfcdfeb845dca695ef7ec
christianmasa <cmesakh@ymail.com>
2022-05-18 11:46:40 +0800

    add redux

--- frontend/src/pages/PropertiesPage.jsx
+++ frontend/src/pages/PropertiesPage.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Col,Container,Row} from 'react-bootstrap'
+import {useDispatch, useSelector} from 'react-redux';
+import Spinner from "../components/Spinner"
+import {getProperties,reset} from "../features/properties/propertySlice"
 
 export default function PropertiesPage() {
-  return (
 
-    <Container>
-        <Row>
-            <Col className="mg-top">
-                <h1>Properties will be displayed here</h1>
-            </Col>
-        </Row>
-    </Container>
-  )
+    const {properties,isLoading,isSuccess} = useSelector((state) => state.properties)
+    const dispatch = useDispatch()
+    useEffect(() =>{
+        dispatch(getProperties())
+    },[dispatch])
+
+    if(isLoading){
+        return <Spinner/>
+    }
+    return (
+
+        <Container>
+            <Row>
+                <Col className="mg-top">
+                    <h1>Properties will be displayed here</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
