307c33f461b47fa986a09d37d444be4e045936cd
christianmasa <cmesakh@ymail.com>
2022-05-18 15:47:27 +0800

    finishing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-bootstrap';
 import HomePage from './pages/HomePage';
 import PropertiesPage from './pages/PropertiesPage';
+import LoginPage from './pages/LoginPage';
 import {Footer} from './components/Footer';
 import {Header}  from './components/Header'
 import "react-toastify/dist/ReactToastify.css";
 import NotFound from './components/NotFound';
+import RegisterPage from './pages/RegisterPage';
+import ActivationPage from './pages/ActivationPage';
 
 const  App = () => {
   return (
@@ -17,6 +20,9 @@ const  App = () => {
         <Routes>
           <Route path="/" element={<HomePage/>}></Route>
           <Route path="/properties" element={<PropertiesPage/>}></Route>
+          <Route path="/login" element={<LoginPage/>}></Route>
+          <Route path="/register" element={<RegisterPage/>}></Route>
+          <Route path="/activation/:uid/:token" element={<ActivationPage/>}></Route>
           <Route path='*' element={<NotFound/>}></Route>
         </Routes>
       <ToastContainer/>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import { Container,Nav,Navbar,NavDropdown } from 'react-bootstrap'
 import {GiHouse} from 'react-icons/gi'
 import {LinkContainer} from 'react-router-bootstrap'
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa"
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+import { logout, reset } from "../features/auth/authSlice";
+
 
 
 export const Header = () => {
+
+    const navigate = useNavigate();
+	const dispatch = useDispatch();
+
+	const { user } = useSelector((state) => state.auth);
+
+	const logoutHandler = () => {
+		dispatch(logout());
+		dispatch(reset());
+		navigate("/");
+	};
+
   return (
     <header>
         <Navbar fixed="top" bg="dark" expand="lg" variant="dark" collapseOnSelect>
@@ -27,11 +44,21 @@ export const Header = () => {
                         <Nav.Link >Properties</Nav.Link>
                     </LinkContainer>
 
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
+                    { user ? (
+                         <NavDropdown title={user.first_name ? user.first_name: "welcome"} id="username">
+                             <LinkContainer to="/profile">
+                                <NavDropdown.Item >Profile</NavDropdown.Item>
+                             </LinkContainer>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                        </NavDropdown>
+                    ): (
+                        <LinkContainer to="/login">
+                            <Nav.Link>
+                                <FaSignInAlt/>Login
+                            </Nav.Link>
+                        </LinkContainer>
+                    )}
                 </Nav>
             </Navbar.Collapse>
         </Container>
