0dfbc350d5fa4141f7bae709e54cac57a699a416
christianmasa <cmesakh@ymail.com>
2022-05-18 12:52:30 +0800

    modify properties

--- frontend/src/pages/PropertiesPage.jsx
+++ frontend/src/pages/PropertiesPage.jsx
@@ -2,27 +2,57 @@ import React, { useEffect } from 'react'
 import {Col,Container,Row} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux';
 import Spinner from "../components/Spinner"
-import {getProperties,reset} from "../features/properties/propertySlice"
 
+import { toast } from "react-toastify";
+import {getProperties,reset} from "../features/properties/propertySlice"
+import Title from "../components/Title"
+
+import Property from "../components/Property";
 export default function PropertiesPage() {
 
-    const {properties,isLoading,isSuccess} = useSelector((state) => state.properties)
+    const { properties, isLoading, isError, message } = useSelector(
+		(state) => state.properties
+	);
     const dispatch = useDispatch()
-    useEffect(() =>{
-        dispatch(getProperties())
-    },[dispatch])
+    useEffect(() => {
+		if (isError) {
+			toast.error(message, { icon: "😭" });
+		}
+		dispatch(getProperties());
+	}, [dispatch, isError, message]);
+
 
     if(isLoading){
         return <Spinner/>
     }
     return (
-
-        <Container>
-            <Row>
-                <Col className="mg-top">
-                    <h1>Properties will be displayed here</h1>
-                </Col>
-            </Row>
-        </Container>
+        <>
+			<Title title="Our Properties Catalog" />
+			<Container>
+				<Row>
+					<Col className="mg-top text-center">
+						<h1>Our Catalog of properties</h1>
+						<hr className="hr-text" />
+					</Col>
+				</Row>
+				{
+					<>
+						<Row className="mt-3">
+							{properties.map((property) => (
+								<Col
+									key={property.id}
+									sm={12}
+									md={6}
+									lg={4}
+									xl={3}
+								>
+									<Property property={property} />
+								</Col>
+							))}
+						</Row>
+					</>
+				}
+			</Container>
+		</>
     )
 }
